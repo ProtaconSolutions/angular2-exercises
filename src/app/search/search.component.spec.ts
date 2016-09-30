50f081eec49221fc06ad4e7497ea87d1305df905
henri <henri.ihalainen@gofore.com>
2016-09-30 08:50:06 +0300

    Improved SearchComponent tests

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -9,7 +9,7 @@ import {By} from '@angular/platform-browser';
 
 describe('Component: Search', () => {
   let fixture: any;
-  let spy: any;
+  let service: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,47 +19,61 @@ describe('Component: Search', () => {
     });
 
     fixture = TestBed.createComponent(SearchComponent);
+    service = fixture.debugElement.injector.get(SearchService);
 
-    let service = fixture.debugElement.injector.get(SearchService);
-    spy = spyOn(service, 'search').and.returnValue(Observable.of([
-      {html_url: 'url', full_name: 'name'}
+    spyOn(service, 'search').and.returnValue(Observable.of([
+      {html_url: 'http://github.com/gofore', full_name: 'GoforeRepo'}
     ]));
+  });
+
+  it('should have a header', () => {
+    expect(fixture.nativeElement.textContent).toContain('GitHub Repository Search');
+    expect(fixture.debugElement.query(By.css('h1')).nativeElement.textContent).toBe('GitHub Repository Search');
   });
 
   it('should query search service', fakeAsync(() => {
     fixture.detectChanges();
-    fixture.componentInstance.control.setValue('search term');
+    fixture.componentInstance.control.setValue('Gofore');
     tick(500);
     fixture.detectChanges();
 
     const el = fixture.debugElement.query(By.css('a')).nativeElement;
 
-    expect(el.attributes['href'].value).toBe('url');
-    expect(el.text).toBe('name');
-    expect(spy.calls.argsFor(0)[0]).toBe('search term');
+    expect(el.attributes['href'].value).toBe('http://github.com/gofore');
+    expect(el.text).toBe('GoforeRepo');
+    expect(service.search).toHaveBeenCalledWith('Gofore');
   }));
 
   it('should not invoke service again if within debounce time', fakeAsync(() => {
     fixture.detectChanges();
-    fixture.componentInstance.control.setValue('search term');
-    fixture.componentInstance.control.setValue('search term2');
+    fixture.componentInstance.control.setValue('Gofore');
+    fixture.componentInstance.control.setValue('Goforeeeeeeeee');
     tick(500);
-    expect(spy.calls.count()).toBe(1);
+    expect(service.search).toHaveBeenCalledTimes(1);
   }));
 
   it('should not invoke service with the same input', fakeAsync(() => {
     fixture.detectChanges();
-    fixture.componentInstance.control.setValue('search term');
+    fixture.componentInstance.control.setValue('Gofore');
     tick(500);
-    fixture.componentInstance.control.setValue('search term');
+    fixture.componentInstance.control.setValue('Gofore');
     tick(500);
-    expect(spy.calls.count()).toBe(1);
+    expect(service.search).toHaveBeenCalledTimes(1);
   }));
 
   it('should not invoke service with an empty input', fakeAsync(() => {
     fixture.detectChanges();
     fixture.componentInstance.control.setValue('');
     tick(500);
-    expect(spy.calls.count()).toBe(0);
+    expect(service.search).not.toHaveBeenCalled();
+  }));
+
+  xit('should not invoke service with an empty input async', async(() => {
+    fixture.detectChanges();
+    fixture.componentInstance.control.setValue('');
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(service.search).not.toHaveBeenCalled();
+    });
   }));
 });
